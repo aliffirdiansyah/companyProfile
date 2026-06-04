75270f45b5bf9d1cc7d0ff680a03209ea4291bfc
alif firdiansyah <aliffirdiansyah5@gmail.com>
2026-06-04 11:38:42 +0700

    test test

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,31 +1,31 @@
-import { Resend } from "resend";
+// import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function POST(req: Request) {
-  try {
-    const { name, email, message } = await req.json();
+// export async function POST(req: Request) {
+//   try {
+//     const { name, email, message } = await req.json();
 
-    await resend.emails.send({
-      from: "Website <onboarding@resend.dev>",
-      to: "emailanda@gmail.com",
-      subject: `Pesan Baru dari ${name}`,
-      html: `
-        <h2>Pesan Baru</h2>
+//     await resend.emails.send({
+//       from: "Website <onboarding@resend.dev>",
+//       to: "emailanda@gmail.com",
+//       subject: `Pesan Baru dari ${name}`,
+//       html: `
+//         <h2>Pesan Baru</h2>
 
-        <p><strong>Nama:</strong> ${name}</p>
-        <p><strong>Email:</strong> ${email}</p>
-        <p><strong>Pesan:</strong></p>
+//         <p><strong>Nama:</strong> ${name}</p>
+//         <p><strong>Email:</strong> ${email}</p>
+//         <p><strong>Pesan:</strong></p>
 
-        <p>${message}</p>
-      `,
-    });
+//         <p>${message}</p>
+//       `,
+//     });
 
-    return Response.json({ success: true });
-  } catch (error) {
-    return Response.json(
-      { success: false, error },
-      { status: 500 }
-    );
-  }
-}
+//     return Response.json({ success: true });
+//   } catch (error) {
+//     return Response.json(
+//       { success: false, error },
+//       { status: 500 }
+//     );
+//   }
+// }
